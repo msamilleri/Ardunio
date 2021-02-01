3992d6b60007f9e074f7306ce25affd91b343b17
Mustafa Şamil İleri <44174659+ileri95@users.noreply.github.com>
2021-02-01 20:55:04 +0300

    Update Voice_CAR.ino

--- Voice_CAR/Voice_CAR.ino
+++ Voice_CAR/Voice_CAR.ino
@@ -31,12 +31,37 @@ pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Distancee=fiind_distance();
-  Serial.print("Distance: ");
-Serial.println(distance);
+Distancee=fiind_distance();
+ while (Serial.available()){ //Okumak için kullanılabilir bayt olup olmadığını kontrol et
+delay(10); //10 milisaniye bekle
+char c = Serial.read(); //Seri okuma
+if (c == '#') {break;
+}// # tespit edildiğinde döngüden çık
+voice += c; //Ses = ses + c Steno
+}
+if (voice.length() > 0) {
+Serial.println(voice);
+if(voice=="*go" || voice=="*ileri"){
+  go();
+}else if(voice=="*back"|| voice=="*geri"){
+  back();
+}else if(voice=="*turn right" || voice=="*sağ dön"){
+  right();
+  
+}else if(voice=="*turn left"||voice=="*sola dön"){
+  left();
+}
 
+voice="";
+}
 
-
+if(0<Distancee || Distancee<10){
+  RGB_color(255, 0, 0); // Red
+}else if(10<Distancee || Distancee<25){
+   RGB_color(255, 255, 0); // Yellow
+}else if(25<Distancee || Distancee<50){
+  RGB_color(0, 255, 0); // Green
+}
 }
 void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
  {
@@ -57,3 +82,28 @@ duration = pulseIn(echoPin, HIGH);
 return distance= duration*0.034/2;
 // Prints the distance on the Serial Monitor
 }
+void go(){
+  digitalWrite(Motor_front_right,HIGH);
+digitalWrite(Motor_front_left, HIGH);
+digitalWrite(Motor_back_right, LOW);
+digitalWrite(Motor_back_left, LOW);
+}
+void back(){
+  digitalWrite(Motor_front_right, LOW);
+digitalWrite(Motor_front_left, LOW);
+digitalWrite(Motor_back_right, LOW);
+digitalWrite(Motor_back_left, HIGH);
+}
+void right(){
+  digitalWrite(Motor_front_right, HIGH);
+digitalWrite(Motor_front_left, LOW);
+digitalWrite(Motor_back_right, LOW);
+digitalWrite(Motor_back_left, HIGH);
+}
+void left(){
+  digitalWrite(Motor_front_right, LOW);
+digitalWrite(Motor_front_left, HIGH);
+digitalWrite(Motor_back_right, HIGH);
+digitalWrite(Motor_back_left, LOW);
+}
+
