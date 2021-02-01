ac61fed67068db2e246baa12bc7c90a362a66be7
Mustafa Şamil İleri <44174659+ileri95@users.noreply.github.com>
2021-02-01 20:20:57 +0300

    Add files via upload

---
+++ Voice_CAR/Voice_CAR.ino
@@ -0,0 +1,26 @@
+//RGB pines
+int red_light=2;
+int green_light=3;
+int blue_light=4;
+//Motor pines
+int Motor_front_right=5;
+int Motor_front_left=6;
+int Motor_back_right=7;
+int Motor_back_left=8;
+String voice; // control for voice
+//HCR-04 pines
+int trigPin=9;  
+int echoin=10;
+//find the distance
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
