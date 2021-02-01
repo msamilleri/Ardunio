afaa87916256c4f6b9843eb9a719a9507d1bb9ce
Mustafa Şamil İleri <44174659+ileri95@users.noreply.github.com>
2021-02-01 20:58:38 +0300

    Update Voice_CAR.ino

--- Voice_CAR/Voice_CAR.ino
+++ Voice_CAR/Voice_CAR.ino
@@ -1,7 +1,7 @@
 //RGB pines
-int red_light=2;
-int green_light=3;
-int blue_light=4;
+int red_light_pin=2;
+int green_light_pin=3;
+int blue_light_pin=4;
 //Motor pines
 int Motor_front_right=5;
 int Motor_front_left=6;
@@ -10,20 +10,20 @@ int Motor_back_left=8;
 String voice; // control for voice
 //HCR-04 pines
 int trigPin=9;  
-int echoin=10;
+int echoPin=10;
 //find the distance
 long duration;
 int distance;
 int Distancee;
 void setup() {
  Serial.begin(9600); //iletişim ayarı
-pinMode(red_light,OUTPUT);
-pinMode(green_light,OUTPUT);
-pinMode(blue_light,OUTPUT)
+pinMode(red_light_pin,OUTPUT);
+pinMode(green_light_pin,OUTPUT);
+pinMode(blue_light_pin,OUTPUT);
 pinMode(Motor_front_right,OUTPUT);
 pinMode(Motor_front_left,OUTPUT);
-pinMode(Motor_back_right,OUTPUT)
-pinMode(Motor_back_left,OUTPUT)
+pinMode(Motor_back_right,OUTPUT);
+pinMode(Motor_back_left,OUTPUT);
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
  
@@ -106,4 +106,3 @@ digitalWrite(Motor_front_left, HIGH);
 digitalWrite(Motor_back_right, HIGH);
 digitalWrite(Motor_back_left, LOW);
 }
-
